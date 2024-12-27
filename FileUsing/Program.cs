6689700cc7f91d0cb1bd222050b120f759276e83
Udaf999999999 <udaf999999999@yandex.ru>
2024-12-27 17:50:36 +0700

    Add classes Drive and Folder

---
+++ FileUsing/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FileUsing;
+
+internal class Program
+{
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+        public void AddFolder(string folderName)
+        {
+            Folders.Add(folderName, new Folder());
+        }
+    }
+    public class Drive
+    {
+        public string Name { get; }
+        public long Space { get; }
+        public long FreeSpace { get; }
+
+        public Drive(string name, long space, long freeSpace)
+        {
+            Name = name;
+            Space = space;
+            FreeSpace = freeSpace;
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
